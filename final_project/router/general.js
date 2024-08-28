9d374e21d007b93a4fdd8ec3d90083915819bdeb
Scott Wofford <skawofford@gmail.com>
2024-08-27 20:29:12 -0400

    Completed task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,27 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if both username and password are provided
+    if (username && password) {
+
+        let userswithsamename = users.filter((user) => {
+            return user.username === username;
+        });
+
+        // Check if the user does not already exist
+        if (userswithsamename == 0) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
